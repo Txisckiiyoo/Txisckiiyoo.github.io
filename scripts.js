2106be9860a9022a5b82e90f651fa7c95f9bda4e
Txisckiiyoo <147490534+Txisckiiyoo@users.noreply.github.com>
2023-11-27 22:57:28 +0100

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', cargarFotos);
+
+function cargarFotos() {
+    // Aquí puedes hacer una solicitud al servidor para obtener las fotos y mostrarlas en el #galeria
+}
